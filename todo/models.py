5fbd3ed6fca2a55ac4c6464e85d3b896caff7344
YahyaNaq <yahya.naqvi123@gmail.com>
2025-04-07 12:11:36 +0500

    initial models, serializer, viewset, etc

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name='%(class)s_created_by', editable=False)
+    updated_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name='%(class)s_updated_by', editable=False)
+
+    class Meta:
+        abstract = True
+        default_permissions = ('add', 'view', 'change', 'delete')
+
+class TodoCategory(BaseModel):
+    name = models.CharField(max_length=64)
+
+class TodoStatus(BaseModel):
+    name = models.CharField(max_length=64)
+
+class Todo(BaseModel):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=2000, null=True, default=None)
+    category = models.ForeignKey(TodoCategory, on_delete=models.PROTECT, null=False)
+    due_date = models.DateField(null=True)
+    is_completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, default=None)
+    is_favourite = models.BooleanField(default=False)
--- todos/settings.py
+++ todos/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+# import environ
+
+# env = environ.Env()
+# environ.Env.read_env()
+
+# import os
+# print("DBBB",os.getenv('DATABASE_NAME'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -81,8 +89,12 @@ WSGI_APPLICATION = 'todos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todos',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
